61cc7716b4eac4ffbfe701539b9469e977d52e4c
r16902 <irosario@fullsail.edu>
2014-11-06 20:43:32 -0500

    Made new changes for Java 1 week 2

--- JavaFundamentalsWeek1/app/src/main/java/com/isaiasrosario/javafundamentalsweek1/MainActivity.java
+++ JavaFundamentalsWeek1/app/src/main/java/com/isaiasrosario/javafundamentalsweek1/MainActivity.java
@@ -1,20 +1,21 @@
 package com.isaiasrosario.javafundamentalsweek1;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import java.util.ArrayList;
 import java.util.HashSet;
-import android.view.inputmethod.InputMethodManager;
 
 
 
@@ -33,8 +34,14 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String works = getString(R.string.itWorks);
+
+        setTitle(works);
+
         Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(this);
+        button2.setOnClickListener(this);
 
         listItems = (ListView) findViewById(R.id.listView);
         arrAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listArray);
@@ -54,6 +61,44 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
                 InputMethodManager.HIDE_NOT_ALWAYS);
 
+        switch (view.getId()){
+
+            case (R.id.button): {
+                addItem();
+                break;
+            }
+
+            case(R.id.button2): {
+                allItems();
+                break;
+            }
+
+        }
+
+    }
+
+    public void getAverage(ArrayList arrayList){
+
+        int iTotal = 0;
+        int iLength = 0;
+        int iCount = 0;
+        String item;
+
+        for (int i = 0; i < arrayList.size(); i++){
+
+            item = listArray.get(i);
+            iLength = item.length();
+            iTotal += iLength;
+            iCount = i +1;
+
+        }
+
+        iLength = iTotal/iCount;
+
+        averageItem.setText("" + iLength);
+    }
+
+    public void addItem(){
         if (enteredText.getText().toString().equals("")){
             Context context = getApplicationContext();
             CharSequence text = ("You Did Not Enter Item");
@@ -81,28 +126,33 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
 
             enteredText.setText("");
         }
+
     }
 
-    public void getAverage(ArrayList arrayList){
-
-        int iTotal = 0;
-        int iLength = 0;
-        int iCount = 0;
+    public void allItems(){
         String item;
+        String allItems = "";
 
-        for (int i = 0; i < arrayList.size(); i++){
+        for (int i = 0; i< listArray.size(); ++i) {
 
             item = listArray.get(i);
-            iLength = item.length();
-            iTotal += iLength;
-            iCount = i +1;
+            allItems += (item + System.getProperty("line.separator"));
 
         }
 
-        iLength = iTotal/iCount;
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("All the Items!");
+        alert.setMessage(allItems);
+        alert.setNegativeButton("Close", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int whichButton){
 
-        averageItem.setText("" + iLength);
+            }
+        });
+
+        alert.show();
+
     }
+
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
