a2fe263df2a17532485bce4078491845d14a45b7
r16902 <irosario@fullsail.edu>
2014-11-13 23:48:56 -0500

    Advances views folder
    New Project Advacned views

---
+++ AdvancedViewsWeek3/app/src/main/java/com/isaiasrosario/advancedviewsweek3/MovieInfo.java
@@ -0,0 +1,24 @@
+package com.isaiasrosario.advancedviewsweek3;
+
+/**
+ * Created by isaiasrosario on 11/13/14.
+ */
+public class MovieInfo {
+
+    private String movieName;
+    private String movieRating;
+    private String movieTime;
+    private String movieDetail;
+
+
+    public String getMovieName() {return movieName;}
+    public String getMovieRating() {return movieRating;}
+    public String getMovieTime() {return movieTime;}
+    public String getMovieDetail() {return movieDetail;}
+
+    public void setMovieName(String movieName) {this.movieName = movieName;}
+    public void setMovieRating(String movieRating) {this.movieRating = movieRating;}
+    public void setMovieTime(String movieTime) {this.movieTime = movieTime;}
+    public void setMovieDetail(String movieDetail) {this.movieDetail = movieDetail;}
+
+}
