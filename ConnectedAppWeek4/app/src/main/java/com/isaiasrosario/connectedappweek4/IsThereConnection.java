6fff6e8360271d32c4ea30002dd2e4697ea9d348
r16902 <irosario@fullsail.edu>
2014-11-20 21:04:40 -0500

    New Connected App files Week 4

---
+++ ConnectedAppWeek4/app/src/main/java/com/isaiasrosario/connectedappweek4/IsThereConnection.java
@@ -0,0 +1,33 @@
+package com.isaiasrosario.connectedappweek4;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by isaiasrosario on 11/20/14.
+ */
+public class IsThereConnection {
+
+    private Context _context;
+
+    public IsThereConnection(Context context){
+
+        this._context = context;
+    }
+    public boolean isConnected() {
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo[] info = connectivity.getAllNetworkInfo();
+        if (info != null){
+            for (int i = 0; i < info.length; i++)
+                if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                {
+                    return true;
+                }
+        }
+        return false;
+
+    }
+
+}
