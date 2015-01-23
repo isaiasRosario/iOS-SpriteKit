a93906c35da9311b4147497042bc717df4087c13
r16902 <irosario@fullsail.edu>
2015-01-22 22:35:59 -0500

    week 3 advance views field

---
+++ Week3AdvanceViews/app/src/main/java/com/example/isaiasrosario/week3advanceviews/Movies.java
@@ -0,0 +1,23 @@
+package com.example.isaiasrosario.week3advanceviews;
+
+/**
+ * Created by isaiasrosario on 1/22/15.
+ */
+public class Movies {
+
+   private String movieName;
+   private String movieRating;
+   private String movieTime;
+   private String movieDetail;
+
+
+   public String getMovieName() {return movieName;}
+   public String getMovieRating() {return movieRating;}
+   public String getMovieTime() {return movieTime;}
+   public String getMovieDetail() {return movieDetail;}
+
+   public void setMovieName(String movieName) {this.movieName = movieName;}
+   public void setMovieRating(String movieRating) {this.movieRating = movieRating;}
+   public void setMovieTime(String movieTime) {this.movieTime = movieTime;}
+   public void setMovieDetail(String movieDetail) {this.movieDetail = movieDetail;}
+}
