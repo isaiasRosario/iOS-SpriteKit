5b856b1bcc10fa797eade2a3c789d53cf9c55469
r16902 <irosario@fullsail.edu>
2015-09-30 19:25:37 -0400

    removed files

--- MultiActivityJava2Week3/app/src/main/java/com/fullsail/multiactivityjava2week3/AddListener.java
+++
@@ -1,8 +0,0 @@
-package com.fullsail.multiactivityjava2week3;
-
-/**
- * Created by isaiasrosario on 3/19/15.
- */
-public interface AddListener {
-   public  void clickListener(String data1, String data2, String data3);
-}
