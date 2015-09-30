9dfb9d0fcd91cd913b61341c72a66646a90960af
r16902 <irosario@fullsail.edu>
2015-09-30 19:25:04 -0400

    removed some files

--- FragmentsJava2Week2/app/src/main/java/com/fullsail/fragmentsjava2week2/FragmentOneListener.java
+++
@@ -1,8 +0,0 @@
-package com.fullsail.fragmentsjava2week2;
-
-/**
- * Created by isaiasrosario on 3/13/15.
- */
-public interface FragmentOneListener {
-   public  void clickListener(String data, String data1, String data2, String data3);
-}
