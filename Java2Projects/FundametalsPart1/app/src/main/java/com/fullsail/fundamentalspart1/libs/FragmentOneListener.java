91cf3b3b0430b5e3074bf0738e18799bfe4245e2
r16902 <irosario@fullsail.edu>
2015-10-04 15:27:03 -0400

    Java 2 Week 1
    New files added

---
+++ Java2Projects/FundametalsPart1/app/src/main/java/com/fullsail/fundamentalspart1/libs/FragmentOneListener.java
@@ -0,0 +1,8 @@
+package com.fullsail.fundamentalspart1.libs;
+
+/**
+ * Created by isaiasrosario on 10/4/15.
+ */
+public interface FragmentOneListener {
+   public  void clickListener(String data, String data1, String data2, String data3);
+}
---
+++ Java2Projects/FundametalsPart1/app/src/main/java/com/fullsail/fundamentalspart1/libs/libs/ManageStorage.java
@@ -0,0 +1,24 @@
+package com.fullsail.fundamentalspart1.libs.libs;
+
+import android.content.Context;
+
+import com.fullsail.fundamentalspart1.libs.FragmentOne;
+
+import java.util.ArrayList;
+
+/**
+ * Created by isaiasrosario on 10/4/15.
+ */
+public class ManageStorage {
+
+   public static void SaveData(ArrayList<String> data, Context context){
+
+
+   }
+
+   public static ArrayList<FragmentOne> LoadData(Context context){
+
+      return null;
+   }
+
+}
