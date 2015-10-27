bd72a44aa9ea5dac1647c2102de9938a2f834b0c
r16902 <irosario@fullsail.edu>
2015-10-26 20:47:47 -0400

    e
    e

--- Java2Projects/FundametalsPart2/app/src/main/java/com/fullsail/fundamentalspart1/libs/FragmentOneListener.java
+++
@@ -1,8 +0,0 @@
-package com.fullsail.fundamentalspart1.libs;
-
-/**
- * Created by isaiasrosario on 10/4/15.
- */
-public interface FragmentOneListener {
-   public  void clickListener(String data, String data1, String data2, String data3);
-}
